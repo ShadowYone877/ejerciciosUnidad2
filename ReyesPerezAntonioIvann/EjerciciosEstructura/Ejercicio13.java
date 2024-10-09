ab2c2be7c806dc9f5cc5c8114fd93736a499e473
unknown <rantonioivann@gmail.com>
2024-10-08 23:49:01 -0600

    Nuevos ejercicios

--- ReyesPerezAntonioIvann/EjerciciosEstructura/Ejercicio13.java
+++ ReyesPerezAntonioIvann/EjerciciosEstructura/Ejercicio13.java
@@ -4,7 +4,6 @@ class Ejercicio13 { // Nombre de la clase
         Scanner op = new Scanner(System.in); //Creamos un objeto Scanner para leer la entrada del usuario//
         System.out.println("Dame un numero: "); //Pedimos al usuario que ingrese un numero//
         int nu = op.nextInt(); //Almacenamos el numero en la variable "nu"//
-        
         for (int i = 0; i <= nu; i++) { //Utilizamos un bucle for para imprimir hasta el numero dado por el usuario//
             if (i % 2 == 0) { //Verificamos si el numero es par//
                 System.out.println(i); //Imprimimos el numero si es par//
--- ReyesPerezAntonioIvann/EjerciciosEstructura/Ejercicio14.java
+++ ReyesPerezAntonioIvann/EjerciciosEstructura/Ejercicio14.java
@@ -1,11 +1,13 @@
 import java.util.Scanner; //Importamos la libreria Scanner//
-class Ejercicio10 { //Nombre de la clase//
+class Ejercicio14 { //Nombre de la clase//
     public static void main(String[] args) { //Metodo principal//
         Scanner op = new Scanner(System.in); //Creamos un objeto Scanner para leer la entrada del usuario//
         System.out.println("Dame un numero: "); //Pedimos al usuario que ingrese un numero//
         int nu = op.nextInt(); //Almacenamos el numero en la variable "nu"//
-        for (int i = 0; i <= nu; i++) { //Utilizamos un bucle for para imprimir desde 0 hasta el numero dado//
-            System.out.println(i); //Imprimimos el resultado//
+        for (int i = 0; i <= nu; i++) { //Utilizamos un bucle for para recorrer los numeros desde 0 hasta el numero dado por el usuario//
+            if (i % 2 != 0) { //Verificamos si el numero es impar//
+                System.out.println(i); //Imprimimos el numero si es impar//
+            }
         }
     }
 }
